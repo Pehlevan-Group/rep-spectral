875594fd6ebeae74e4705ae6e900ed9554f54bfa
yangshengaa <yangshengaa@hotmail.com>
2023-12-14 23:28:06 -0500

    added approx svd to multiclass training

--- src/train_reg_multi.py
+++ src/train_reg_multi.py
@@ -19,9 +19,10 @@ from torch.utils.tensorboard import SummaryWriter
 
 # load file
 from model import (
-    SLP, weights_init, 
+    SLP, weights_init,
+    init_model_right_singular,
     cross_lipschitz_regulerizer, 
-    volume_element_regularizer_autograd, 
+    # volume_element_regularizer_autograd, 
     top_eig_regularizer_autograd,
     top_eig_ub_regularizer_autograd,
     spectral_ub_regularizer_autograd
@@ -50,7 +51,14 @@ parser.add_argument('--sample-size', default=None, type=float,
 parser.add_argument("--m", default=None, type=int, help='vol element specific: keep the top m singular values to regularize only')
 parser.add_argument("--epochs", default=1200, type=int, help='the number of epochs for training')
 parser.add_argument('--burnin', default=600, type=int, help='the period before which no regularization is imposed')
+parser.add_argument('--max-layer', default=None, type=int,
+                    help='the number of max layers to pull information from. None means the entire feature map')
 parser.add_argument('--reg-freq', default=5, type=int, help='the freqency of imposing regularizations')
+
+# iterative singular 
+parser.add_argument('--iterative', action='store_true', default=False, help='True to turn on iterative method')
+parser.add_argument('--tol', default=1e-6, type=float, help='the tolerance for stopping the iterative method')
+parser.add_argument('--max-update', default=10, type=int, help='the maximum update iteration during training')
 
 # logging
 parser.add_argument('--log-epoch', default=100, type=int, help='logging frequency')
@@ -73,6 +81,7 @@ if isinstance(args.hidden_dim, list) and len(args.hidden_dim) == 1: args.hidden_
 
 # set up summary writer
 log_name, base_log_name = get_logging_name(args, 'linear_large')
+if args.reg == "None": log_name = base_log_name
 model_path = os.makedirs(os.path.join(paths['model_dir'], log_name), exist_ok=True)
 writer = SummaryWriter(os.path.join(paths['result_dir'], log_name))
 
@@ -100,6 +109,19 @@ nl = getattr(nn, args.nl)()
 model = SLP(input_dim=784, width=args.hidden_dim, output_dim=10, nl=nl).to(device)
 # initialize gaussian weights
 weights_init(model)
+
+# init model singular values 
+if args.iterative:
+    if args.reg == 'spectral':
+        v_init = init_model_right_singular(model.model, tol=args.tol)
+    elif args.reg == 'eig-ub':
+        v_init = init_model_right_singular(
+            model.feature_map if hasattr(model, 'feature_map') else model.feature_maps[-1],
+            tol=args.tol
+        )
+    print("initialize top right singular direction")
+else:
+    v_init = {}
 
 # get optimizer
 opt = Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
@@ -129,37 +151,12 @@ def train():
             opt.zero_grad()
             y_pred_logits = model(X_train)
             train_loss = loss_fn(y_pred_logits, y_train)
+            
             # record
             total_train_loss += train_loss * len(X_train)
             total_train_acc += y_pred_logits.argmax(dim=-1).eq(y_train).sum()
-            
-            # TODO: change this: only regularize once per epoch
-            # regularization
-            reg_loss = 0
-            if i > args.burnin and i % args.reg_freq == 0:
-                if args.reg == 'cross-lip':
-                    reg_loss = cross_lipschitz_regulerizer(model, X_train, is_binary=False)
-                elif args.reg == 'vol':
-                    reg_loss = volume_element_regularizer_autograd(
-                        X_train, model.feature_map, 
-                        sample_size=args.sample_size, m=args.m, scanbatchsize=args.scanbatchsize
-                    )
-                elif args.reg == 'eig':
-                    reg_loss = top_eig_regularizer_autograd(
-                        X_train, model.feature_map, 
-                        sample_size=args.sample_size, scanbatchsize=args.scanbatchsize
-                    )
-                elif args.reg == 'eig-ub':
-                    reg_loss = top_eig_ub_regularizer_autograd(
-                        X_train, model.feature_map
-                    )
-                elif args.reg == 'spectral':
-                    reg_loss = spectral_ub_regularizer_autograd(
-                        model
-                    )
 
             # step
-            train_loss += reg_loss * args.lam
             train_loss.backward()
             opt.step()
         
@@ -195,6 +192,44 @@ def train():
             pbar.set_postfix(
                 {'tr_loss': train_loss.item(), 'tr_acc': train_acc.item(), 'te_loss': test_loss.item(), 'te_acc': test_acc.item()})
         
+        # regularization
+        # TODO: change this: only regularize once per epoch
+        # regularization
+        reg_loss = None
+        if i > args.burnin and i % args.reg_freq == 0:
+            if args.reg == 'cross-lip':
+                reg_loss = cross_lipschitz_regulerizer(
+                    model, X_train, is_binary=False)
+            # elif args.reg == 'vol':
+            #     reg_loss = volume_element_regularizer_autograd(
+            #         X_train, model.feature_map,
+            #         sample_size=args.sample_size, m=args.m, scanbatchsize=args.scanbatchsize
+            #     )
+            elif args.reg == 'eig':
+                reg_loss = top_eig_regularizer_autograd(
+                    X_train, model.feature_map,
+                    sample_size=args.sample_size, scanbatchsize=args.scanbatchsize
+                )
+            elif args.reg == 'eig-ub':
+                reg_loss = top_eig_ub_regularizer_autograd(
+                    X_train, model.feature_map,
+                    max_layer=args.max_layer,
+                    iterative=args.iterative, v_init=v_init, tol=args.tol, max_update=args.max_update
+                )
+            elif args.reg == 'spectral':
+                reg_loss = spectral_ub_regularizer_autograd(
+                    model,
+                    iterative=args.iterative, v_init=v_init, tol=args.tol, max_update=args.max_update
+                )
+        
+        # regularization
+        if reg_loss is not None:
+            reg_loss *= args.lam
+            opt.zero_grad()
+            reg_loss.backward()
+            opt.step()
+
+
         if i % args.log_model == 0:
             # log final model
             torch.save(model.state_dict(), os.path.join(paths['model_dir'], log_name, f'model_e{i}.pt'))
