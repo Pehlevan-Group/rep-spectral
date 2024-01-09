4bc0c52f16ec8efa77e9720e4647d9bf1a492208
yangshengaa <yangshengaa@hotmail.com>
2024-01-09 15:20:40 -0500

    added convolution layer regularizer

--- src/model/regularizer/__init__.py
+++ src/model/regularizer/__init__.py
@@ -2,5 +2,6 @@ from .cross_lip import *
 from .feat_eig_analytic import *
 from .feat_eig_autograd import *
 from .spectral import *
+from .conv import *
 
 from .utils import init_model_right_singular
--- src/model/regularizer/conv.py
+++ src/model/regularizer/conv.py
@@ -1,9 +1,25 @@
 """
-regularization for convolution layers, assume all inputs shape and kernel shape are squares
+regularization for convolution layers, assume all strides are square
 """
 
 # load packages
-import os
 import torch
 import torch.nn as nn
 
+def top_eig_ub_regularizer_conv(model: nn.Module) -> torch.Tensor:
+    """
+    regularize up to feature map
+    """
+    eigvals = model.get_conv_layer_eigvals()
+    reg_term = sum(eigvals)
+    return reg_term
+
+def spectral_ub_regularizer_conv(model: nn.Module) -> torch.Tensor:
+    """add final linear head regularization"""
+    reg_term = top_eig_ub_regularizer_conv(model)
+
+    # last linear head 
+    W = model.linear.weight 
+    eig = torch.linalg.eigvalsh(W.T @ W).max()
+    reg_term += eig 
+    return reg_term
